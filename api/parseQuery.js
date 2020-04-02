02fa9b947e22c01d277ff89732b5ad9e38bd44bc
Jack Reid <emailjackreid@gmail.com>
2020-04-02 15:21:42 +0100

    express w paging

---
+++ api/parseQuery.js
@@ -0,0 +1,20 @@
+const defaults = {
+  sort: 'DESC',
+  limit: 500,
+  offset: 0,
+};
+
+function parseQuery(req, res, next) {
+  console.log(req.params);
+  const config = {
+    sort: req.query.sort || defaults.sort,
+    limit: req.query.limit || defaults.limit,
+    offset: req.query.offset || defaults.offset,
+  };
+
+  req.config = config;
+
+  next();
+}
+
+module.exports = parseQuery;
