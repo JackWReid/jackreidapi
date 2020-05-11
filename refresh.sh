383eb58790fb9e60ea3731c47f5567a66b685a80
Jack Reid <hello@jackreid.xyz>
2020-05-11 11:44:07 +0100

    refresh script update

--- refresh.sh
+++ refresh.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 date +"%Y-%m-%dT%T"
-/usr/local/bin/docker-compose kill api
+git checkout -f
+git pull origin HEAD
 /usr/local/bin/docker-compose up -d --build api books_update films_update articles_update pocket_update
 date +"%Y-%m-%dT%T"
