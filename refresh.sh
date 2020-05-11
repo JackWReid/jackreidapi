7a53ca0ab71f322afdd2f7eb7b8fff35f40ce4e2
Jack Reid <hello@jackreid.xyz>
2020-05-11 15:36:11 +0100

    careful api

--- refresh.sh
+++ refresh.sh
@@ -3,5 +3,5 @@
 date +"%Y-%m-%dT%T"
 git checkout -f
 git pull origin HEAD
-/usr/local/bin/docker-compose up -d --build api books_update films_update articles_update pocket_update
+/usr/local/bin/docker-compose up -d --build books_update films_update articles_update pocket_update
 date +"%Y-%m-%dT%T"
