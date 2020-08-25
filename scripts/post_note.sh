bef642421b051ae13159c6f89ffebe2bb5e99275
Jack Reid <hello@jackreid.xyz>
2020-08-25 10:04:42 +0000

    note script

---
+++ scripts/post_note.sh
@@ -0,0 +1,15 @@
+git pull --no-edit origin master
+cd /root/jackreidapi/site
+
+date_str=$(date +"%Y-%m%dT%T")
+/home/linuxbrew/.linuxbrew/bin/hugo new note/$date_str.md
+new_note_file="content/note/$date_str.md"
+echo "---" > $new_note_file
+echo "date: ${date_str}" >> $new_note_file
+echo "title: $1" >> $new_note_file
+echo "---" >> $new_note_file
+cat $new_note_file
+
+git add .
+git commit -am "new note"
+git push origin master
