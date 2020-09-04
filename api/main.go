ffe41d81aea0c030ad7a071826f5b7c865098ca7
Jack Reid <hello@jackreid.xyz>
2020-09-04 14:38:02 +0100

    goin

--- api/main.go
+++ api/main.go
@@ -1,20 +1,66 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
 )
 
+type Book struct {
+	Title       string `json:"title"`
+	Author      string `json:"author"`
+	Image       string `json:"image"`
+	DateUpdated string `json:"date_updated"`
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	password := os.Getenv("PG_PASS")
+	log.Print(password)
+	connStr := fmt.Sprintf("user=jack dbname=personal host=jackreid.xyz password=%s sslmode=require", password)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/books/reading", func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT title, author, image, date_updated FROM books WHERE status = 'reading'")
+		books := []Book{}
+		defer rows.Close()
+		for rows.Next() {
+			book := Book{}
+			err = rows.Scan(&book.Title, &book.Author, &book.Image, &book.DateUpdated)
+			if err != nil {
+				panic(err)
+			}
+			books = append(books, book)
+		}
+		b, _ := json.Marshal(books)
+		s := string(b)
+		fmt.Fprintf(w, s)
 	})
 
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
+	http.HandleFunc("/books/read", func(w http.ResponseWriter, r *http.Request) {
+		rows, err := db.Query("SELECT title, author, image, date_updated FROM books WHERE status = 'read'")
+		books := []Book{}
+		defer rows.Close()
+		for rows.Next() {
+			book := Book{}
+			err = rows.Scan(&book.Title, &book.Author, &book.Image, &book.DateUpdated)
+			if err != nil {
+				panic(err)
+			}
+			books = append(books, book)
+		}
+		b, _ := json.Marshal(books)
+		s := string(b)
+		fmt.Fprintf(w, s)
 	})
 
+	log.Print("api.jackreid.xyz listening on :3000")
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
